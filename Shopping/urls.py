51c37490eda4f88b690800d9ac3fb3fbac0de18a
Aayush Kumar Singh <malemeaayushkumarsingh@gmail.com>
2021-07-29 18:29:32 +0530

    Update urls.py

--- Shopping/urls.py
+++ Shopping/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
                   path('', views.index, name='Home'),
                   path('shop/', include("shop.urls")),
                   path('blog/', include("blog.urls")),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
